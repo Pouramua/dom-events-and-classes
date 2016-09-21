841c7386256a14e8fac623976b3b3fa6333e4a45
Pouramua <poukupenga@gmail.com>
2016-09-22 10:43:28 +1200

    hide button

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ function bindEventListeners (dots) {
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
     dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide);
   }
 }
 
@@ -25,14 +26,15 @@ function makeGreen (evt) {
 // CREATE FUNCTION makeBlue HERE
 function makeBlue (evt) {
   evt.target.classList.toggle('blue')
-  updateCounts()
+  updateCounts();
 }
 // CREATE FUNCTION hide HERE
 function hide(evt){
-  evt.preventDefault();
   evt.target.classList.toggle('invisible');
   updateCounts();
 }
+
+
 
 function updateCounts () {
   var totals = {
